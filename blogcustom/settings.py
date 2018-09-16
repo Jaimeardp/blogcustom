c95873adbabef9d4bf6485f9b3f1215ed823bcc7
Jaimeardp <jaimeardp@gmail.com>
2018-09-16 00:05:30 +0000

    last commit today

--- blogcustom/settings.py
+++ blogcustom/settings.py
@@ -143,6 +143,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = '/home/Jaimeardp/blogcustom/'
 
 
 LOGIN_REDIRECT_URL = 'pages:pages'
